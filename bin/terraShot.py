cb29d869f2b9f203d0985209b1f018d9a527d12b
vonKloppen <mariusz.nowatzki@gmail.com>
2022-12-21 09:51:07 +0100

    Add logging to syslog for terraShot

--- bin/terraShot.py
+++ bin/terraShot.py
@@ -1,13 +1,22 @@
 #!/bin/python3
 
-import os
+import os, syslog
 from gpiozero import LED
+from time import sleep
+
+### VARIABLES ###
 
 light = LED(24)
+interval = 180
 
-light.on()
+###
 
-os.system('raspistill -q 100 -br 50 -rot 270 -roi 0,0.1,0.8,0.8 -w 500 -h 500 -n -o /mnt/terraControl/view.jpg')
+while True:
 
-light.off()
-
+  sleep(interval)
+  syslog.syslog(syslog.LOG_INFO, "Turning light ON")
+  light.on()
+  syslog.syslog(syslog.LOG_INFO, "Taking picture")
+  os.system('raspistill -q 100 -br 50 -rot 270 -roi 0,0.1,0.8,0.8 -w 500 -h 500 -n -o /mnt/terraControl/view.jpg')
+  syslog.syslog(syslog.LOG_INFO, "Turning light OFF")
+  light.off()
