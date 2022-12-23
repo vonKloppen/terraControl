c4c0c51ea3208ff51067af17988ac781f001baee
vonKloppen <mariusz.nowatzki@gmail.com>
2022-12-23 19:26:05 +0100

    Add migrate script from csv to sqlite

---
+++ bin/migrate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -z "$1" || -z "$2" ]]
+
+    then
+
+        echo -e "Usage: migrate.sh file_CSV file_SQLITE"
+        exit 1
+
+fi
+
+IFS=$'\t\n'
+
+for entry in $(cat "$1")
+
+    do
+
+        data=`echo "$entry" | awk -F "," '{print $1}'`
+        temp=`echo "$entry" | awk -F "," '{print $2}'`
+        epoch=`echo "$data" | xargs -d \n date +%s -d`
+        sqlite3 "$2" 'insert into temperature values("'$data'", '$epoch', '$temp');'
+
+done
+
